a9e1446e0a6ea0402d013af08c6431c4c7ca5391
jagrut1810 <jagrut.arora@gmail.com>
2019-04-02 16:27:17 -0700

    adding comments to the file

--- index.js
+++ index.js
@@ -1,51 +1,49 @@
-const express = require('express'),
-     http = require('http');
-
-const morgan = require('morgan');
+const express = require('express');
+const http = require('http'); //http code module
+const morgan = require('morgan'); // required to serve static file in the application
+const bodyParser = require('body-parser'); //parses the body of the request and adds to the object req.body
 
 const hostname = 'localhost';
 const port = 3000;
 
-const app = express();
+const app = express(); //tell that our application will use the express node module
 
-const bodyParser = require('body-parser');
-
-app.use(morgan('dev'));
-
-app.use(express.static(__dirname + '/public'));
-
-app.use((req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
-});
-
+app.use(morgan('dev')); //development version - so print additional info
 app.use(bodyParser.json());
 
+//Express REST API methods
+// /dishes -> Endpoint
+
+//This code will be executed every time an API is called
 app.all('/dishes', (req,res,next) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  next();
+  next(); //operation not finished -> continue looking for the next one (e.g. PUT request)
 });
 
+//Specific to the kind of request
+//GET - get the info
 app.get('/dishes', (req,res,next) => {
     res.end('Will send all the dishes to you!');
 });
 
+//POST - new dish
 app.post('/dishes', (req, res, next) => {
  res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
 });
 
+//PUT - Update existing dish
 app.put('/dishes', (req, res, next) => {
   res.statusCode = 403;
   res.end('PUT operation not supported on /dishes');
 });
 
+//DELETE - delete a dish
 app.delete('/dishes', (req, res, next) => {
     res.end('Deleting all dishes');
 });
 
+//Doing the same thing for dishID
 app.get('/dishes/:dishId', (req,res,next) => {
     res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
 });
@@ -65,8 +63,16 @@ app.delete('/dishes/:dishId', (req, res, next) => {
     res.end('Deleting dish: ' + req.params.dishId);
 });
 
-const server = http.createServer(app);
+app.use(express.static(__dirname + '/public')); //serve static files from this location - __dirname means root
+
+app.use((req, res, next) => { //req -> request ; res -> response
+  res.statusCode = 200; //Status code for the response -> if 200 then everything is ok
+  res.setHeader('Content-Type', 'text/html'); //response type formatted in HTML
+  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
+
+const server = http.createServer(app); //creating the server
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`); //use `` since we need to use a variable inside;
 });
